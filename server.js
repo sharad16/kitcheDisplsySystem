c046543859896b96c1687d38935158ed464dbe94
sharad16 <sharadvanjari5@gmail.com>
2018-06-10 04:29:43 -0700

    make it better

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var mongoose = require('mongoose');
+console.log(__dirname + '/client/index.html',path);
+var config = require('./server/config/dbConfig');
+
+var kitchenController = require('./server/controller/kitchen');
+
+
+//configuration
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser());
+// app.use(express.static(path.join(__dirname + 'client')));
+app.use(express.static(path.join(__dirname, './client')));
+app.use(kitchenController);
+
+app.listen(config.PORT_NUMBER,function(){
+	console.log('server running on port '+config.PORT_NUMBER);
+  console.log(__dirname + '/client/index.html');
+})
